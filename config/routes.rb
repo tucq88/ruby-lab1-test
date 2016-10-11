4cdf38d1d0474ff80d5b45a58a1e5eeb6dfa6bb5
Tucq <tucq88@gmail.com>
2016-10-11 12:57:15 +0000

    Mystone 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'photos/index'
+
+  get 'photos/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
+  resources :photos
 end
